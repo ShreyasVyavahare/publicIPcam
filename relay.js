5c4b89baca2823e71e0d606b0eeeea34ba514cd4
ShreyasVyavahare <shreyasvy12@gmail.com>
2026-06-19 18:49:45 +0530

    second push

--- relay.js
+++ relay.js
@@ -21,7 +21,7 @@ const path     = require('path');
 const fs       = require('fs');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const STREAM_KEY   = process.env.STREAM_KEY   || '';
 const ALLOWED_IPS  = (process.env.ALLOWED_IPS || '').split(',').map(s => s.trim()).filter(Boolean);
 
